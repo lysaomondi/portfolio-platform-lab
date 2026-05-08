d16077cfcd516f3a2c9b3584eeb294547a5ba541
lysa omondi <lisabethkidangasi@gmail.com>
2026-05-08 15:08:40 +0300

    images

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,23 +1,23 @@
 const projectsData =[
   {
     id: 1,
-    title: "Project 1",
-    description: "Description of the project",
-    image: "https://via.placeholder.com/80",
+    title: "Developer Portfolio",
+    description: "Personal portfolio website showcasing projects and skills",
+    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
 
   },
   {
     id: 2,
-    title: "Project 2",
-    description: "Description of the project",
-    image: "https://via.placeholder.com/80",
+    title: "SkinCare Website",
+    description: "Understand your skin better",
+    image: "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9",
 
   },
   {
     id: 3,
-    title: "Project 3",
-    description: "Description of the project",
-    image: "https://via.placeholder.com/80",
+    title: "Beauty Products Platform",
+    description: "Showcasing beauty products",
+    image: "https://images.unsplash.com/photo-1596462502278-27bfdc403348",
 
   },
   
